9029444d7bfe72c67896ff5ae8cedf4d2c9afa74
Zdendaaa <138407224+Zorvoan@users.noreply.github.com>
2025-11-03 23:38:02 +0100

    Add files via upload

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'phone' => 'nullable|string|max:255',
+            'email' => 'required|email',
+            'message' => 'required|string',
+        ]);
+
+        Contact::create([
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'email' => $request->email,
+            'message' => $request->message,
+        ]);
+
+        return redirect()->back()->with('success', 'Thank you! We will contact you soon.');
+    }
+}
